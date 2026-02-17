27c81e544b5f29a998f3d4650fc0c5316b8c4d38
nathanjsoares <nathansoares@gmail.com>
2026-02-17 11:48:17 -0300

    Product find by Id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO(){
+    }
+
+    public ProductDTO(Product entitie) {
+        id = entitie.getId();
+        name = entitie.getName();
+        description = entitie.getDescription();
+        price = entitie.getPrice();
+        imgUrl = entitie.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
