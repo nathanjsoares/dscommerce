73d232c14874458d5fc8ec0fb6275c0a69a256e6
Nathan Soares <nathansoares@email.com>
2026-02-27 16:59:05 -0300

    User Role e seed

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dscommerce.entities;
+
+import java.util.Objects;
+
+//import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    //@Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authority);
+    }
+}
